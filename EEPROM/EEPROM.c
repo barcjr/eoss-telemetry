5bd09a48bbfe6c00188e1eb23509e64d2ad5a163
Nick O'Dell <nickodell@gmail.com>
2011-10-21 23:46:28 -0600

    Add EEPROM library and EEPROM test program.

---
+++ EEPROM/EEPROM.c
@@ -0,0 +1,191 @@
+#include <p18cxxx.h>
+#include <i2c.h>
+
+unsigned char calcI2CAddress(unsigned char base, unsigned char chip_num, unsigned char block_select)
+{
+	return base | (chip_num << 2) | (block_select << 1);
+}
+
+
+/********************************************************************
+*	Function Name:	EEByteWrite										*
+*	Return Value:	error condition status							*
+*	Parameters:		EE memory device control, address and data		*
+*					bytes.											*
+*	Description:	Something of a misnomer, write as many bytes as you want
+*																	*
+********************************************************************/
+signed char EEByteWrite_mod(unsigned char control, unsigned long address, unsigned char *data_src, unsigned char len)
+{
+	
+	unsigned char bytes[] = {control, (address >> 8) & 0xFF, address & 0xFF};
+	unsigned char i;
+	unsigned char blockSel;
+	signed char err;
+	
+	printf("EEByteWrite_mod\r\n");
+	IdleI2C();							// ensure module is idle
+	StartI2C();							// initiate START condition
+	while(SSPCON2bits.SEN);
+		//printf("+");			// wait until start condition is over
+	if(PIR2bits.BCLIF)				 // test for bus collision
+	{
+		return -1;					// return with Bus Collision error 
+	}
+	
+	blockSel = (address >> 16) & 0x1;	//Bit 16 is which block
+	
+	bytes[0] |= (blockSel << 1);        //Set block select bit in control byte
+	
+	for(i = 0; i < 3; i++)
+	{
+		printf("Byte to write to I2C: %02x\r\n", bytes[i]);
+		err = WriteI2C(bytes[i]);		// write byte - R/W bit should be 0
+		//printf("done\r\n");
+		if(err)
+		{
+			//printf("err: %d\r\n", err);
+			StopI2C();
+			return -3;				// set error for write collision
+		}
+		//printf("wait ack\r\n");
+		if(SSPCON2bits.ACKSTAT)
+		{
+			//printf("ackstat\n");
+			StopI2C();
+			return -2;				// return with Not Ack error condition
+		}
+	}
+	for(i = 0; i < len; i++)
+	{
+		//printf("Byte[%d] to write to I2C: %02x\r\n", i, data_src[i]);
+		err = WriteI2C(data_src[i]);
+		//printf("done\r\n");
+		if(err)		// write byte - R/W bit should be 0
+		{
+			//printf("err: %d\r\n", err);
+			StopI2C();
+			return -3;				// set error for write collision
+		}
+		//printf("wait ack\r\n");;
+		if(SSPCON2bits.ACKSTAT)
+		{
+			//printf("ackstat\n");
+			StopI2C();
+			return -2;				// return with Not Ack error condition
+		}
+		//data_src++;
+	}
+	//printf("done\n");
+
+	//IdleI2C();						// ensure module is idle	
+	StopI2C();							// send STOP condition
+	while(SSPCON2bits.PEN);			// wait until stop condition is over 
+	if(PIR2bits.BCLIF)				// test for bus collision
+	{
+		return -1;					// return with Bus Collision error 
+	}
+	
+	Delay10KTCYx(10);
+	return 0;						// return with no error
+	
+}
+
+/********************************************************************
+*	Function Name:	EEByteWrite										*
+*	Return Value:	error condition status							*
+*	Parameters:		EE memory device control, address and data		*
+*					bytes.											*
+*	Description:	Write single data byte to I2C EE memory			*
+*					device. This routine can be used for any I2C	*
+*					EE memory device, which only uses 2 byte of		*
+*					address data as in the 24FC1026					*
+*																	*
+********************************************************************/
+signed char EERandomRead_mod(unsigned char control, unsigned long address, unsigned char *data, unsigned char len)
+{
+	
+	unsigned char bytes[] = {control, (address >> 8) & 0xFF, address & 0xFF};
+	unsigned char i;
+	unsigned char blockSel;
+	
+	signed char err;
+	StartI2C();							// initiate START condition
+	while(SSPCON2bits.SEN);
+		//printf("+");			// wait until start condition is over 
+	if(PIR2bits.BCLIF)				 // test for bus collision
+	{
+		return -1;					// return with Bus Collision error 
+	}
+	
+	blockSel = (address >> 16) & 0x1;	//Bit 16 is which block
+	
+	bytes[0] |= (blockSel << 1);        //Set block select bit in control byte
+	
+	//printf("Reading from eprom\r\n");
+	for(i = 0; i < 3; i++)
+	{
+		//printf("EERandomRead Byte to write to I2C: %02x\r\n", bytes[i]);
+		err = WriteI2C(bytes[i]);
+		//printf("done\r\n");
+		if(err)
+		{
+			//printf("err: %d\r\n", err);
+			StopI2C();
+			return -3;				// set error for write collision
+		}
+		//printf("wait ack\r\n");;
+		if(SSPCON2bits.ACKSTAT)
+		{
+			//printf("ackstat\n");
+			StopI2C();
+			return -2;				// return with Not Ack error condition
+		}
+	}
+	
+	AckI2C();
+	StartI2C();
+
+	while(SSPCON2bits.PEN);			// wait until stop condition is over 
+	if(PIR2bits.BCLIF)				// test for bus collision
+	{
+		return -1;					// return with Bus Collision error 
+	}
+	control |= 0x01;
+	
+	//printf("EERandomRead: Byte to write to I2C: %02x\r\n", control);
+
+	err = WriteI2C(control);
+	//printf("done\r\n");
+	if(err)
+	{
+		//printf("err: %d\r\n", err);
+		StopI2C();
+		return -3;				// set error for write collision
+	}
+	//printf("wait ack\r\n");
+	if(SSPCON2bits.ACKSTAT)
+	{
+		//printf("ackstat\n");
+		StopI2C();
+		return -2;				// return with Not Ack error condition
+	}
+	
+//	while(!SSPSTATbits.BF) {
+//		//printf("SSPBUF = %02x\r\n", SSPBUF);
+//	}
+	for(i = 0; i < len-1; i++) {
+		*data++ = ReadI2C();
+		AckI2C();
+	}
+	*data++ = ReadI2C();          	//Read final byte
+	NotAckI2C();
+
+	StopI2C();							// send STOP condition
+	while(SSPCON2bits.PEN);			// wait until stop condition is over 
+	if(PIR2bits.BCLIF)				// test for bus collision
+	{
+		return -1;					// return with Bus Collision error 
+	}
+	return 0;						// return with no error
+}
---
+++ EEPROM/EEPROM_Test.c
@@ -0,0 +1,253 @@
+#include				<p18f4520.h>
+#include				<stdio.h>
+#include				<i2c.h>
+#include				<delays.h>
+#include				"EEPROM.c"
+
+#pragma config OSC=INTIO67, WDT=OFF, LVP=OFF, DEBUG=ON
+
+
+//** Defines ************************************************************
+#define		CONTROL_BASE		0xa0
+#define		CHIP_NUM			0				//Should be 0-3
+#define		BLOCK_SELECT		0				//More of a placeholder than anything.
+
+#define		LED					LATAbits.LATA0
+
+/************************************************************************
+*
+* Purpose:		Configures USART module for TX operation
+* Passed:		None
+* Returned:	 	SPBRG, TXSTA, RCSTA
+* Note:			Asynchronous Mode
+*
+************************************************************************/
+void openTxUsart(void)
+{
+		// TX UART Configuration
+		
+		SPBRG = 12;					// Set baud = 9600
+		TXSTAbits.SYNC = 0;			// Asynchronous mode
+		RCSTAbits.SPEN = 1;			// Serial port enabled
+		TXSTAbits.BRGH = 0;			// Low Speed
+		TXSTAbits.TXEN = 1;			// Enable transmission 
+}
+
+/************************************************************************
+*
+* Purpose:		Goes through device memory, checking to see if there's a
+*				byte NOT EQUAL to needle. If so, print a warning.
+* Returned:	 	0 if good, -1 if read error, -2 if byte is not what we expected.
+*
+************************************************************************/
+#define BLOCK_SIZE 32
+	
+signed char EEPROM_memcheck(unsigned char device, unsigned char needle)
+{
+	signed char err;
+	unsigned long address;
+	unsigned char temp[BLOCK_SIZE];
+	unsigned char i;
+	
+	
+	printf((const far rom char*) "Full memory check start of device %02x.\r\n", device);
+	
+	for(i = 0; i < BLOCK_SIZE; i++)
+	{
+		// Junk to make sure it fails if RandomRead doesn't actually
+		// set this for some reason.
+		temp[i] = 0x48 ^ i;
+	}
+	
+	for(address = 0x0000; address < 0x10000; address += BLOCK_SIZE)
+	{
+		err = EERandomRead_mod(device, address, &temp[0], BLOCK_SIZE);
+		//printf((const far rom char*) "blah\r\n");
+		//printf((const far rom char*) "ERROR in EERandomRead_mod: %d\r\n", err);
+		if(err)
+		{
+			printf((const far rom char*) "ERROR in EERandomRead_mod: %d\r\n", err);
+			return -1;
+		}
+		for(i = 0; i < BLOCK_SIZE; i++)
+		{
+			if(temp[i] != needle)
+			{
+				printf((const far rom char*) "ERROR: Byte %lx is %02x rather than expected %02x\r\n", address, temp[i], needle);
+				return -2;
+			}
+			//printf(".");
+		}
+	}
+	
+	printf((const far rom char*) "Full memory check successful.\r\n");
+	return 0;
+}
+
+/************************************************************************
+*
+* Purpose:		Goes through device memory, writing needle.
+* Returned:	 	0 if good, -1 if read error.
+*
+************************************************************************/
+signed char EEPROM_memwrite(unsigned char device, unsigned char needle)
+{
+	signed char err;
+	unsigned long address;
+	unsigned char temp_write[BLOCK_SIZE];
+	unsigned char i;
+	
+	for(i = 0; i < BLOCK_SIZE; i++)
+	{
+		temp_write[i] = needle;
+	}
+	
+	printf((const far rom char*) "Full memory write start of device %02x.\r\n", device);
+	
+	for(address = 0x0000; address < 0xFFFF; address += BLOCK_SIZE)
+	{
+		err = EEByteWrite_mod(device, address, &temp_write[0], BLOCK_SIZE);
+		//printf((const far rom char*) "ERROR in EEByteWrite_mod: %d\r\n", err);
+		if(err)
+		{
+			printf((const far rom char*) "ERROR in EEByteWrite_mod: %d\r\n", err);
+			return -1;
+		}
+		//printf((const far rom char*) ".");
+	}
+	printf((const far rom char*) "Full memory write successful.\r\n");
+	return 0;
+}
+
+/************************************************************************
+*
+* Purpose:		Goes through device memory, dumping it to UART
+* Returned:	 	0 if good, -1 if read error.
+*
+************************************************************************/
+signed char EEPROM_memdump(unsigned char device)
+{
+	signed char err;
+	unsigned long address;
+	unsigned char temp[BLOCK_SIZE];
+	unsigned char i;
+	
+	
+	printf((const far rom char*) "Full memory dump start of device %02x.\r\n", device);
+	
+	for(address = 0x0000; address < 0x10000; address += BLOCK_SIZE)
+	{
+		err = EERandomRead_mod(device, address, &temp[0], BLOCK_SIZE);
+		//printf((const far rom char*) "blah\r\n");
+		//printf((const far rom char*) "ERROR in EERandomRead_mod: %d\r\n", err);
+		if(err)
+		{
+			printf((const far rom char*) "ERROR in EERandomRead_mod: %d\r\n", err);
+			return -1;
+		}
+		for(i = 0; i < BLOCK_SIZE; i++)
+		{
+			printf((const far rom char*) "%02x", temp[i]);
+			//printf(".");
+		}
+	}
+	
+	printf((const far rom char*) "Full memory dump successful.\r\n");
+	return 0;
+}
+
+/*** Notes *************************************************************
+*
+* Memory is sub-divided into 512K blocks. The first block is 0x0000 to 
+* 0xFFFF and the second block is 0x10000 to 0x1FFFF.
+*
+***********************************************************************/
+void main()
+{
+		// Variables
+		int i, counter, ack;
+		unsigned char control;
+		unsigned char read_dest[32];
+		unsigned char write_src[32];
+		
+		OSCCON = 0x70;			// 8 MHz OSC
+		while(!OSCCONbits.IOFS);
+		
+		TRISAbits.TRISA0 = 0;			// LED
+		
+		// I2C
+		TRISCbits.TRISC3 = 1;			// SCL
+		TRISCbits.TRISC4 = 1;			// SDA
+		
+		// UART
+		TRISCbits.TRISC6 = 1;			// TX
+		TRISCbits.TRISC7 = 1;			// RX
+		
+		OpenI2C(MASTER, SLEW_ON);
+		SSPADD = 4;          // rate = Fosc/4*(SSPADD + 1)       rate = 8 MHz/20 = 400 kHz
+		//SSPADD = 1;        // SSPADD = Fosc/(4*rate) - 1       if rate = 1 MHz, 8/4 - 1 = 1
+		//SSPADD = 19;                     // 100 kHz SSPADD = 8 MHz/(4*100kHz) - 1 = 19
+		//SSPADD = 199;        // 10 kHz
+		
+		openTxUsart();
+		
+		printf((const far rom char*) "=========================\r\n");
+		printf((const far rom char*) "=========RESTART=========\r\n");
+		printf((const far rom char*) "=========RESTART=========\r\n");
+		printf((const far rom char*) "=========RESTART=========\r\n");
+		printf((const far rom char*) "=========================\r\n");
+			
+		Delay10KTCYx(100);
+		
+
+		
+ 		
+		printf((const far rom char*) "MASTER: %02x, SSPADD: %d\r\n", MASTER, SSPADD);
+		
+		control = calcI2CAddress(CONTROL_BASE, CHIP_NUM, 0);
+		//EEPROM_memwrite(control, 0xDD);
+		//EEPROM_memcheck(control, 0xFF);
+		EEPROM_memdump(control);
+		
+		control = calcI2CAddress(CONTROL_BASE, CHIP_NUM, 1);
+		//EEPROM_memwrite(control, 0xFF);
+		//EEPROM_memcheck(control, 0xFF);
+	
+		
+#if 0
+		printf((const far rom char*) "Test initiated.\r\n");
+		
+		for(i = 0; i < 32; i++) {
+			write_src[i] = 0xFF;
+			read_dest[i] = 0x00;
+		}
+		printf((const far rom char*) "EEByteWrite_mod: %d\r\n", EEByteWrite_mod(control, 0x0000, &write_src[0], 32));
+		printf((const far rom char*) "EERandomRead_mod: %d\r\n", EERandomRead_mod(control, 0x0000, &read_dest[0], 32));
+		for(i = 0; i < 32; i++) {
+			printf((const far rom char*) "Byte read: %02x\r\n", read_dest[i]);
+		}
+		
+#endif	 
+		/*if(!ack)
+		{
+				printf("Test successful, the byte was sent.\r\n");
+				
+				for(i = 0; i < 5; i++)
+				{
+						LED = 1;
+						Delay10KTCYx(10);
+						LED = 0;
+						Delay10KTCYx(10);
+				}
+		}
+		else
+		{
+				printf((const far rom char*) "Test unsuccessful...\r\n");
+				LED = 1;
+		}*/
+		/*for(i = 0; i < 10; i++)
+		{
+			Delay10KTCYx(100);
+		}*/
+		while(1);
+}
