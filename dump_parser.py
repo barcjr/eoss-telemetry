a3a10b3b7d474515a5d4e2455d5e8b37a26f041a
Nick O'Dell <nickodell@gmail.com>
2011-10-24 17:12:30 -0600

    Import python program to interpret eeprom dumps.
    Run it by putting the output of EEPROM_memdump into dump.txt. Make sure the
    dump is the first 500+ hex digit sequence in the file.

---
+++ dump_parser.py
@@ -0,0 +1,44 @@
+import binascii
+import re
+from sensor_calibration_test import callibrate
+
+dump = open("dump.txt").read()
+
+# Look for at least 500 hex digits
+data = re.search("[0-9a-fA-F]{500,}", dump).group(0)
+data = binascii.unhexlify(data)
+data = map(ord, data)
+
+def press_parse(block):
+    up = reduce(lambda x, y: (x << 8) + y, block[0:3])
+    return block[3:], up
+
+def temp_parse(block):
+    ut = reduce(lambda x, y: (x << 8) + y, block[0:2])
+    return block[2:], ut
+
+addr = 0
+errors = []
+while True:
+    block = data[addr:addr+16]
+    end = True
+    # Is this block entirely 0xFF?
+    # If so, end the program
+    for b in block:
+        if b != 255:
+            end = False
+            break
+    if end:
+        break
+    #print ut, up
+    while len(block) > 5:
+        block, up = press_parse(block)
+        block, ut = temp_parse(block)
+        
+        print calibrate(ut, up)
+    if block[0] != 0:
+        errors.append(block[0])
+        print block[0]
+    addr += 16
+print errors
+
